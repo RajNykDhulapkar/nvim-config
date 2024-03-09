c7f3df27228bef4fae50c68e55d8c5ee344e21c7
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2024-03-09 19:05:39 +0530

    keymap: kill word with ctrl+Backspace

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,9 @@
 -- Add any additional keymaps here
 
 local keymap = vim.keymap
--- local opts = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true }
 
 -- select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
+-- delete word with Ctrl+Backspace
+keymap.set("i", "<C-H>", "<C-w>", opts)
