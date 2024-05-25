10e2a72ff9603430fc80003f97e41e9507bcbb70
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2024-05-25 16:26:06 +0530

    feat: add python, clangd, nvim-platformio

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -5,4 +5,13 @@ return {
     cmd = "Copilot",
     event = "InsertEnter",
   },
+  -- platformio
+  {
+    "anurag3301/nvim-platformio.lua",
+    dependencies = {
+      { "akinsho/nvim-toggleterm.lua" },
+      { "nvim-telescope/telescope.nvim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+  },
 }
