3a26a93e9aa98bf69084d2ea24ceb40bb4d28ca5
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2024-08-06 00:49:33 +0530

    fix: keymap ctrl backspace del word

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,4 +8,4 @@ local opts = { noremap = true, silent = true }
 -- select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
 -- delete word with Ctrl+Backspace
-keymap.set("i", "<C-H>", "<C-w>", opts)
+keymap.set("i", "<C-h>", "<C-w>", opts)
