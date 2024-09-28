46e9b5938054cf5a468815fa449203ecedca1c01
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2024-09-28 10:37:57 +0530

    feat: ts, tailwind lsp setup

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -5,6 +5,25 @@ return {
     cmd = "Copilot",
     event = "InsertEnter",
   },
+  -- parrot
+  {
+    "frankroeder/parrot.nvim",
+    dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
+    -- optionally include "rcarriga/nvim-notify" for beautiful notifications
+    config = function()
+      require("parrot").setup({
+        -- Providers must be explicitly added to make them available.
+        providers = {
+          anthropic = {
+            api_key = os.getenv("ANTHROPIC_API_KEY"),
+          },
+          github = {
+            api_key = os.getenv("GITHUB_TOKEN"),
+          },
+        },
+      })
+    end,
+  },
   -- platformio
   {
     "anurag3301/nvim-platformio.lua",
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -10,10 +10,10 @@ return {
       "TmuxNavigatePrevious",
     },
     keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   },
@@ -25,4 +25,35 @@ return {
       { "<c-p>", "<Plug>(VM_Create_Cursors_From_Pattern)" },
     },
   },
+  -- alpha.nvim
+  { import = "lazyvim.plugins.extras.ui.alpha" },
+  -- icons
+  {
+    "echasnovski/mini.icons",
+    config = function()
+      require("mini.icons").setup()
+    end,
+  },
+  -- inlay hints
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+  -- mini.surround
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  }
 }
